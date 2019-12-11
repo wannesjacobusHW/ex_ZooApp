475f1da2a9dcec8f6e01af74cd495b49fb4560ce
wannesjacobusHW <wannes.jacobus@student.howest.be>
2019-12-11 14:02:09 +0100

    Add contructors

--- Zoo.Lib/Lion.cs
+++ Zoo.Lib/Lion.cs
@@ -12,11 +12,17 @@ namespace Zoo.Lib
     {  
         public Sex Sex { get; set; }
         public string Name { get; set; }
+        public int Age { get; set; }
 
-        public Lion(string name, Sex sex)
+        public Lion(string name) : this(name, Sex.F) { }
+        public Lion(string name, Sex sex) : this(name, sex, 0) { }
+        public Lion(string name, Sex sex, int age)
         {
             Name = name;
             Sex = sex;
+            Age = age;
         }
+
+
     }
 }
